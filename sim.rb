2133d52b5e91d12af6ce86f7573da27c65b0cafb
Jeff Zohrab <jzohrab@gmail.com>
2015-11-14 11:07:06 -0500

    Moved questions to file.

--- sim.rb
+++ sim.rb
@@ -1,29 +1,9 @@
 # Maybe simulate some tasks to do them from command line, b/c doing
 # them in PacketTracer can be a hassle.
 
-instruction = <<ENDI
-configure ipv6 eigrp on router, with int f0/0 (ipv6 a::1/48)
-Use AS #100
-ENDI
-
-question = <<ENDQ
-!Set up ipv6 routing
-R1(config)#ipv6 unicast-routing 
-!Configure the interface
-R1(config)#int f0/0
-R1(config-if)#ipv6 address a::1/48
-R1(config-if)#no shut
-%LINK-5-CHANGED: Interface FastEthernet0/0, changed state to up
-%LINEPROTO-5-UPDOWN: Line protocol on Interface FastEthernet0/0, changed state to up
-R1(config-if)#exit
-!Set up eigrp (sys 100)
-R1(config)#ipv6 router eigrp 100
-R1(config-rtr)#no shut
-R1(config-rtr)#int f0/0
-R1(config-if)#ipv6 eigrp 100
-R1(config-if)#exit
-R1(config)#exit
-ENDQ
+require 'yaml'
+qs = YAML.load(File.read(File.join("questions", "questions.yml")))
+qs.map! { |q| q["text"] = File.read(File.join("questions", q["file"])); q }
 
 def test_input_line(lin)
   prompt, command = lin.strip.split("#")
@@ -47,9 +27,10 @@ def test_input_line(lin)
   end
 end
 
-puts instruction
-qs = question.split("\n")
-qs.each do |lin|
+q = qs[1]
+puts q["question"]
+lines = q["text"].split("\n")
+lines.each do |lin|
   if lin.include?('#')
     test_input_line(lin)
   else
